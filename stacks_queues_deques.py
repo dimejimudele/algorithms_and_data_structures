5bdc034e6166a5677cd183724ae62f03c6b23725
dimejimudele <mudeledimeji@gmail.com>
2019-03-06 18:54:25 +0100

    Stack implemented

--- stacks_queues_deques.py
+++ stacks_queues_deques.py
@@ -1,18 +1,27 @@
 #Linear data structures
 
+#Stack: Last-in, first-out
 class stack:
-  def __init__(self):
-    self.items = []
+    def __init__(self):
+        self.items = []
 
-  def isEmpty(self):
-    return self.items == []
+    def __str__(self):
+        return str(self.items)
 
-  def push(self, item):
-    return self.items.append(item)
-  
-  def peek(self):
-    return self.items[-1]
+    def isEmpty(self):
+        return self.items == []
 
-  def size(self):
-    return len(self.items)
+    def push(self, item):
+        return self.items.append(item)
 
+    def peek(self):
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
+
+s = stack()
+s.push(5)
+s.push(10)
+print(s)
